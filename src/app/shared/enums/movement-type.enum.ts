0112b6571465768ee02cbdd0ccfa79dbf003d348
Ana Troiano <anac.troiano@gmail.com>
2026-08-17 21:12:21 -0300

    improvement: improve view components

--- src/app/shared/enums/movement-type.enum.ts
+++ src/app/shared/enums/movement-type.enum.ts
@@ -1,31 +1,31 @@
 export enum MovementType {
-    ENTRY = 'ENTRY',
-    MANUAL_EXIT = 'MANUAL_EXIT',
-    SALE_EXIT = 'SALE_EXIT'
+  ENTRY = 'ENTRY',
+  MANUAL_EXIT = 'MANUAL_EXIT',
+  SALE_EXIT = 'SALE_EXIT'
 }
 
 export function getMovementTypeName(type: MovementType): string {
-    switch (type) {
-        case MovementType.ENTRY:
-            return 'Entrada';
-        case MovementType.MANUAL_EXIT:
-            return 'Saída manual';
-        case MovementType.SALE_EXIT:
-            return 'Saída por venda';
-        default:
-            return '';
-    }
+  switch (type) {
+    case MovementType.ENTRY:
+      return 'Entrada';
+    case MovementType.MANUAL_EXIT:
+      return 'Saída manual';
+    case MovementType.SALE_EXIT:
+      return 'Saída por venda';
+    default:
+      return '';
+  }
 }
 
 export function getMovementTypeBadge(type: MovementType): string {
-    switch (type) {
-        case MovementType.ENTRY:
-            return 'success';
-        case MovementType.MANUAL_EXIT:
-            return 'danger';
-        case MovementType.SALE_EXIT:
-            return 'info';
-        default:
-            return '';
-    }
+  switch (type) {
+    case MovementType.ENTRY:
+      return 'success';
+    case MovementType.MANUAL_EXIT:
+      return 'danger';
+    case MovementType.SALE_EXIT:
+      return 'warning';
+    default:
+      return '';
+  }
 }
