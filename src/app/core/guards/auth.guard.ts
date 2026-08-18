84bf8d24a8d7aa2f02311572b532c95e88b89f69
Ana Troiano <anac.troiano@gmail.com>
2026-08-18 20:56:36 -0300

    chore: format core files

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,7 +1,6 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
-import { TokenService } from '../services/token.service';
+import {inject} from '@angular/core';
+import {CanActivateFn, Router} from '@angular/router';
+import {TokenService} from '../services/token.service';
 
 export const authGuard: CanActivateFn = () => {
 
