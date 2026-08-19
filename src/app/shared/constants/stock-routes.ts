0b7015574a687127dc899868a0c487dd142ab697
Ana Troiano <anac.troiano@gmail.com>
2026-08-18 21:00:34 -0300

    chore: format shared > route files

--- src/app/shared/constants/stock-routes.ts
+++ src/app/shared/constants/stock-routes.ts
@@ -2,7 +2,5 @@ export const STOCK_ROUTES = {
   LIST: '/stocks',
   NEW: '/stocks/new',
   DETAILS: (id: number | string) =>
-    `/stocks/${id}/view`,
-  EDIT: (id: number | string) =>
-    `/stocks/edit/${id}`
+    `/stocks/${id}/view`
 };
